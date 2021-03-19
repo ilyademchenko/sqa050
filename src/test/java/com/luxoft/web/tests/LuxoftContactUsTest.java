66aae27b87812e6468a52a9def796e58145bda1b
Ilya <spinoyzah@gmail.com>
2021-03-19 08:18:10 +0600

    fix tests

--- src/test/java/com/luxoft/web/tests/LuxoftContactUsTest.java
+++ src/test/java/com/luxoft/web/tests/LuxoftContactUsTest.java
@@ -41,27 +41,6 @@ public class LuxoftContactUsTest extends AbstractTest {
                 .verifyPhoneNumberEquals(cityToSelect, expectedPhoneNumber);
     }
 
-    @ParameterizedTest
-    @CsvFileSource(resources = "/phoneNumberData.csv", numLinesToSkip = 1)
-    public void verifyContactPhoneNumber(String countryToSelect, String cityToSelect, String expectedPhoneNumber) {
-        driver.findElement(By.cssSelector("div.header__menu > ul > li:nth-child(6) > a")).click();
-//        driver.findElement(By.linkText("CONTACT US")).click();
-        WebElement contactUsFooterButton = driver.findElement(By.cssSelector(".footer__connect .career-banner-btn"));
-        wait.until(elementToBeClickable(contactUsFooterButton)).click();
-        wait.until(elementToBeClickable(By.xpath("//li[@class='tabs__nav__item ']/h2[contains(text(),'Call')]" +
-                "/span[contains(text(),'us')]/../.."))).click();
-        wait.until(elementToBeClickable(By.cssSelector("#tab-2 > div > div > div > div:nth-child(1) > span"))).click();
-        //country
-        wait.until(elementToBeClickable(By.xpath(String.format("//li[contains(@class,'select2-results__option') and contains(text(),'%s')]", countryToSelect)))).click();
-        //city
-        wait.until(elementToBeClickable(By.xpath("//div[@style='' and @class='city call-select']"))).click();
-        wait.until(elementToBeClickable(By.xpath(String.format("//li[contains(@class,'select2-results__option') and contains(text(),'%s')]", cityToSelect)))).click();
-
-        String phoneNumber = wait.until(visibilityOfElementLocated(By.xpath("//*[contains(@class,'call-results')]/h5[contains(text(), '" + cityToSelect + "')]/following-sibling::h2"))).getText();
-        Assertions.assertEquals(expectedPhoneNumber, phoneNumber, "Phone number");
-        driver.navigate().to(baseUrl);
-    }
-
     @AfterAll
     public static void tearDown() {
         cleanUp();
--- src/test/java/com/luxoft/web/tests/LuxoftMainPageTest.java
+++ src/test/java/com/luxoft/web/tests/LuxoftMainPageTest.java
@@ -1,17 +1,31 @@
 package com.luxoft.web.tests;
 
+import com.luxoft.web.page.LuxoftContactUsPage;
 import com.luxoft.web.page.LuxoftHomePage;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class LuxoftMainPageTest extends AbstractTest {
 
-    private LuxoftHomePage homePage;
+    private static LuxoftHomePage homePage;
+
+    @BeforeAll
+    public static void init() {
+        setUp();
+        homePage = new LuxoftHomePage(driver);
+    }
 
     @Test
     public void verifyCopyright() {
         homePage = new LuxoftHomePage(driver);
         Assertions.assertTrue(
                 homePage.copyrightInfo.getText().contains("2021"), "Current year in copyright");
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        cleanUp();
     }
 }
