99278345ca278b90910f73e4f1489bc601afa2d9
Ilya <spinoyzah@gmail.com>
2021-03-19 10:45:06 +0600

    add display test name

--- src/test/java/com/luxoft/web/tests/LuxoftContactUsTest.java
+++ src/test/java/com/luxoft/web/tests/LuxoftContactUsTest.java
@@ -3,15 +3,12 @@ package com.luxoft.web.tests;
 import com.luxoft.web.config.TestWatcher;
 import com.luxoft.web.page.LuxoftContactUsPage;
 import com.luxoft.web.page.LuxoftHomePage;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 @ExtendWith(TestWatcher.class)
 public class LuxoftContactUsTest extends AbstractTest {
@@ -33,7 +30,8 @@ public class LuxoftContactUsTest extends AbstractTest {
                 .selectCallUsTab();
     }
 
-    @ParameterizedTest
+    @DisplayName("Check Call us phone numbers")
+    @ParameterizedTest(name = "Verify '{1}' city from '{0} country has proper phone number = '{2}''")
     @CsvFileSource(resources = "/phoneNumberData.csv", numLinesToSkip = 1)
     public void verifyContactPhoneNumberRefactoredSteps(String countryToSelect, String cityToSelect, String expectedPhoneNumber) {
         contactUsPage
--- src/test/java/com/luxoft/web/tests/LuxoftMainPageTest.java
+++ src/test/java/com/luxoft/web/tests/LuxoftMainPageTest.java
@@ -2,10 +2,9 @@ package com.luxoft.web.tests;
 
 import com.luxoft.web.page.LuxoftContactUsPage;
 import com.luxoft.web.page.LuxoftHomePage;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.time.Year;
 
 public class LuxoftMainPageTest extends AbstractTest {
 
@@ -18,10 +17,11 @@ public class LuxoftMainPageTest extends AbstractTest {
     }
 
     @Test
+    @DisplayName("Verify that copyright contains current year")
     public void verifyCopyright() {
         homePage = new LuxoftHomePage(driver);
         Assertions.assertTrue(
-                homePage.copyrightInfo.getText().contains("2021"), "Current year in copyright");
+                homePage.copyrightInfo.getText().contains(Year.now().toString()), "Current year in copyright");
     }
 
     @AfterAll
